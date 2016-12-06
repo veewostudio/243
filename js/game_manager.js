a7aecaf7ac19a90b34e4b82f875c310e11e60adb
veewostudio <benjohnsonmathsteacher@gmail.com>
2016-12-06 20:25:33 +0000

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 1 : 3;
+    var value = Math.random() < 0.999 ? 1 : 243;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -154,7 +154,7 @@ GameManager.prototype.move = function (direction) {
           self.score += merged.value;
 
           // The mighty 2048 tile
-          if (merged.value === 243) self.won = true;
+          if (tile.value === 243) self.won = true;
         }
         
 	 else if (!tile.merged){
